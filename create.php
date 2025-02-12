fe7459378feeaa8f7ad75b375d7384075680c84f
Diego Silva <diegosilva.dev13@gmail.com>
2025-02-12 10:54:09 -0300

    Create create.php

---
+++ create.php
@@ -0,0 +1,10 @@
+<?php
+// Código para inserir um novo contato no banco de dados
+// ...
+?>
+<form method="post" action="create.php">
+  Nome: <input type="text" name="nome"><br>
+  Email: <input type="email" name="email"><br>
+  Telefone: <input type="tel" name="telefone"><br>
+  <input type="submit" value="Salvar">
+</form>
